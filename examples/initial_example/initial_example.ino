a19fd40f78bc1a0d21082b4a9c0dd9c6b9d87fe0
JoeArchibold <jarchibold@mines.edu>
2024-03-05 02:22:01 +0000

    normal program

--- examples/initial_example/initial_example.ino
+++ examples/initial_example/initial_example.ino
@@ -3,14 +3,14 @@
 
 LightGrid lg;
 
-const int FADE_LEN = 64;
+const int FADE_LEN = 128;
 int lastX[FADE_LEN+1] = {-1};
 int lastY[FADE_LEN+1] = {-1};
 
 void setup() {
   // put your setup code here, to run once:
   lg = LightGrid();
-  lg.setBrightness(15);
+  lg.setBrightness(5);
 
   // for(int x = 1; x <= 64; x++) {
   //   for(int y = 1; y<=8; y++) {
@@ -35,7 +35,7 @@ void loop() {
       // delay(100);
 
       lg.drawPixel(lastX[FADE_LEN], lastY[FADE_LEN], CRGB(45,45,45));
-      // lg.clearPixel(lastX[FADE_LEN], lastY[FADE_LEN]);
+      //lg.clearPixel(lastX[FADE_LEN], lastY[FADE_LEN]);
       for(int i = FADE_LEN; i > 0 ; i--) {
         lastX[i] = lastX[i-1];
         lastY[i] = lastY[i-1];
